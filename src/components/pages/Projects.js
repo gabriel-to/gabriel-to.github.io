88be2dbb8f695bcc7009ee6e0a711cb529e175f0
Gabriel <Gabriel@DESKTOP-R33DKU2>
2021-09-06 14:42:37 -0400

    tft-roller project update

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import '../../App.css';
+
 function Projects() {
     return (
         <div className="projects">
-            <h1> TEST</h1>            
+            <div className="tft-roller-container">
+                <h1>tft-roller</h1>
+                <img src="/tft-roller.jpg" alt="tft-roller screenshot"/>
+                <p id="project-text">
+                tft-roller is a tool made to calculate the odds of obtaining a certain unit given a certain level, the amount of gold, the unit cost and the number of units taken 
+                in the game Teamfight Tactics made by Riot Games.
+                </p>
+             </div>
         </div>
     )
 }
