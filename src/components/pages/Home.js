b482eb9de1bbf16b2c4c593a7ae9c5d5e09a9e06
Gabriel <Gabriel@DESKTOP-R33DKU2>
2021-09-07 12:34:30 -0400

    formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,9 +2,13 @@ import '../../App.css';
 import React from 'react';
 
 function Home() {
+    
     return (
         <div className='home'>
-            <p>Hello, welcome to my website, I am Gabriel To, graduated in Computer Science at Université de Montréal. Here you can see my past projects, my resume and where to contact me.</p>
+            <p>
+                Hello, welcome to my website, I am Gabriel To, graduated in Computer Science at Université de Montréal. Here you can see my past projects, my resume and where to
+                contact me.
+            </p>
 
         </div>
     )
